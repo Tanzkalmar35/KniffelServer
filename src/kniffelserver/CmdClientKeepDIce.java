45c319e8f7a3e85408c4cdb92c91c997cace1ebf
Fabian Holler <fabianholler2005@gmail.com>
2023-04-26 10:10:41 +0200

    rolldice implemented, now working on keepdice

--- src/kniffelserver/CmdClientKeepDIce.java
+++ src/kniffelserver/CmdClientKeepDIce.java
@@ -1,9 +1,9 @@
 package kniffelserver;
 
-import java.net.Socket;
-
 import gamedb.GameData;
 import gamedb.GameDataException;
+
+import java.net.Socket;
 
 public class CmdClientKeepDIce extends CmdClient {
 
@@ -13,8 +13,7 @@ public class CmdClientKeepDIce extends CmdClient {
 
     @Override
     String excuteLocalCmd(String parameter) throws GameDataException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'excuteLocalCmd'");
+        return "";
     }
-    
+
 }
